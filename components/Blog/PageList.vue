54249dac0b7ced3746b4332d43c8ae6269025b9e
Sutan Gading Fadhillah Nasution <sutan.gnst@gmail.com>
2019-09-01 15:15:08 +0700

    add no matching posts found fallback

--- components/Blog/PageList.vue
+++ components/Blog/PageList.vue
@@ -84,6 +84,9 @@
                   </div>
                 </Card>
               </div>
+              <div v-if="posts.length === 0" class="col-sm-12 my-3">
+                <h2>🙁 Oops, no matching posts found!</h2>
+              </div>
             </div>
           </div>
         </div>
