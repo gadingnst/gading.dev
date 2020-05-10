ddab2a2e0b129412a6da6af59abcdb34227ab69d
Sutan Nasution <sutan.gnst@gmail.com>
2020-05-11 05:37:34 +0700

    modified support components

--- components/Base/Support.vue
+++ components/Base/Support.vue
@@ -1,43 +1,39 @@
 <template>
   <div class="my-3">
     <Button
+      v-for="(support, idx) in supports"
+      :key="idx"
+      :href="support.link"
       tag="a"
       target="_blank"
-      href="https://trakteer.id/sutanlab"
       size="lg"
       class="d-inline-flex my-1"
-      bg-color="#BE1E2D"
+      :bg-color="support.color"
     >
       <client-only>
-        <md-ice-cream-icon w="20px" h="20px" />
+        <component :is="support.icon" w="20px" h="20px" />
       </client-only>
-      &nbsp;Trakteer
-    </Button>
-    <Button
-      tag="a"
-      target="_blank"
-      href="https://karyakarsa.com/sutanlab"
-      type="danger"
-      size="lg"
-      class="d-inline-flex my-1"
-      bg-color="#EB5757"
-    >
-      <client-only>
-        <md-heart-icon w="20px" h="20px" />
-      </client-only>
-      &nbsp;KaryaKarsa
+      &nbsp;{{ support.name }}
     </Button>
   </div>
 </template>
 
 <script>
 import MdHeartIcon from 'vue-ionicons/dist/md-heart.vue'
+import MdCafeIcon from 'vue-ionicons/dist/md-cafe.vue'
 import MdIceCreamIcon from 'vue-ionicons/dist/md-ice-cream.vue'
 import Button from '~/components/Argon/Button'
 
 export default {
   components: {
-    Button, MdHeartIcon, MdIceCreamIcon
-  }
+    Button, MdCafeIcon, MdHeartIcon, MdIceCreamIcon
+  },
+  data: () => ({
+    supports: [
+      { name: 'Ko-fi', link: 'https://ko-fi.com/sutanlab', color: '#ff5f5f', icon: 'md-cafe-icon' },
+      { name: 'Trakteer', link: 'https://trakteer.id/sutanlab', color: '#BE1E2D', icon: 'md-ice-cream-icon' },
+      { name: 'Karyakarsa', link: 'https://karyakarsa.com/sutanlab', color: '#eb5757', icon: 'md-heart-icon' }
+    ]
+  })
 }
 </script>
