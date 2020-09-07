37684b9b20d54837b301ee6e8df12fd7059f16c0
Gading <sutan.gnst@gmail.com>
2020-09-08 01:40:51 +0700

    update resume

--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,7 @@
                     tag="a"
                     target="_blank"
                     rel="noreferrer"
-                    href="/media/resume-sutan-gading-fadhillah-nasution.pdf"
+                    href="/media/resume.pdf"
                     type="info"
                     size="sm"
                     class="mr-4"
