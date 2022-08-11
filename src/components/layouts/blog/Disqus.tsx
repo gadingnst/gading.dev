0f1614868911748f82a13921303f80667c22d455
Gading Nasution <sutan.gnst@gmail.com>
2022-08-11 20:03:38 +0700

    feat: disqus switch theme

--- src/components/layouts/blog/Disqus.tsx
+++ src/components/layouts/blog/Disqus.tsx
@@ -1,6 +1,8 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import { DiscussionEmbed } from 'disqus-react';
 import { DISQUS_SHORTNAME } from '@/utils/config';
+import useAppTheme from '@/hooks/stores/useAppTheme';
+import { useUpdated } from '@/hooks';
 
 interface Props {
   url: string;
@@ -10,9 +12,19 @@ interface Props {
 }
 
 const Disqus: FunctionComponent<Props> = (props) => {
+  const [theme] = useAppTheme();
+  const [isDark, setIsDark] = useState(theme.current === 'light' ? false : true);
+
+  useUpdated(() => {
+    setTimeout(() => {
+      setIsDark(!isDark);
+    }, 250);
+  }, [theme.current]);
+
   return (
     <div className="container max-w-5xl mt-40 mx-auto">
       <DiscussionEmbed
+        key={+isDark}
         shortname={DISQUS_SHORTNAME}
         config={
           {
