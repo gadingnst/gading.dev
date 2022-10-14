ce2ab5ee5c1adb9ef5e20b608f5766fe9e06b93d
Gading Nasution <sutan.gnst@gmail.com>
2022-10-15 01:39:12 +0700

    refactor: split function

--- src/utils/helpers/locales.ts
+++ src/utils/helpers/locales.ts
@@ -1,32 +1,44 @@
 import { I18n, I18nLocales } from '@/types/contents';
 
-type LocalesToContent = {
+type ObjectContents = {
+  [locale in I18nLocales]: string
+};
+
+type I18nLocalesToContent = {
   [locale in I18nLocales]: Record<string, string>
 };
 
-type ContentToLocales = {
-  en: string;
-  id: string;
-};
-
-type I18nContentLocales<K extends string> = Record<K, ContentToLocales>;
+type I18nContentToLocales<K extends string> = Record<K, ObjectContents>;
 
 /**
- * convert passed (contents -> locales) based to (locales -> contents based)
- * @param data (content -> locales) data
+ * convert passed (contents->locales based) to (locales->contents based)
+ * @param data (content->locales based) data
  */
-function createContentLocales<T extends string>(data: I18nContentLocales<T>): (locale: I18nContentLocales<T>|string) => Record<keyof typeof data, string> {
-  const classificationLocales: LocalesToContent = Object.entries(data)
-    .reduce((acc, [key, contentLocales]) => {
-      Object.keys(I18n).forEach((locale) => {
-        const content = (contentLocales as ContentToLocales)[locale as I18nLocales];
-        acc[locale as I18nLocales][key] = content;
+function generateLocaleBasedContents<T extends string>(data: I18nContentToLocales<T>): I18nLocalesToContent {
+  const classificationLocales: I18nLocalesToContent = Object.entries(data)
+    .reduce((accumulator, [key, contentLocales]) => {
+      Object.keys(I18n).forEach((lcl) => {
+        const locale = lcl as I18nLocales;
+        const content = (contentLocales as ObjectContents)[locale];
+        if (accumulator?.[locale]) {
+          accumulator[locale][key] = content;
+        } else {
+          accumulator[locale] = { [key]: content };
+        }
       });
-      return acc;
-    }, { en: {}, id: {} } as LocalesToContent);
-  return (locale: I18nContentLocales<T>|string) => (
-    classificationLocales[locale as I18nLocales]
-  );
+      return accumulator;
+    }, {} as I18nLocalesToContent);
+  return classificationLocales;
+}
+
+/**
+ * curry `generateLocaleBasedContents` with another function
+ * @param data (content->locales based) data
+ */
+function createContentLocales<T extends string>(data: I18nContentToLocales<T>):
+  (locale: I18nContentToLocales<T>|string) => Record<keyof typeof data, string> {
+  const locales = generateLocaleBasedContents(data);
+  return (locale: I18nContentToLocales<T>|string) => locales[locale as I18nLocales];
 }
 
 export default createContentLocales;
