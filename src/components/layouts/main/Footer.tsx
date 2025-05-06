4b2d80ffcb5fc5396f90047f63ccaedc14985053
Gading Nasution <account@gading.dev>
2025-05-07 01:08:33 +0700

    feat: update

--- src/components/layouts/main/Footer.tsx
+++ src/components/layouts/main/Footer.tsx
@@ -94,9 +94,6 @@ const Footer: FunctionComponent<Props> = (props) => {
           {AUTHOR_NAME}
         </Link>
         <div className="dark:text-white mt-8">
-          <Link className="inline-block text-primary" href="https://analytics.gading.dev/share/OBNvcvLy/Gading's%20Hideout">
-            Analytics
-          </Link>
           <span className="inline-block mx-4">&bull;</span>
           <Link className="inline-block text-primary" href="/docs/colors">
             Colors System
