66c5ec4f555c288f512e13efa335bc486c618fad
Gading Nasution <account@gading.dev>
2025-06-30 22:56:23 +0700

    refactor(AppTheme): remove unnecessary string replacement in getServerAppTheme
    The cookie value no longer contains quotes that need to be removed, so the extra processing step can be eliminated

--- src/packages/libs/AppTheme/getServerAppTheme.ts
+++ src/packages/libs/AppTheme/getServerAppTheme.ts
@@ -5,8 +5,7 @@ import { APP_THEME_KEY } from '@/packages/libs/AppTheme/constants';
 async function getServerAppTheme() {
   const cookies = Cookies('server');
   const appTheme = await cookies.get(APP_THEME_KEY);
-  const appThemeValue = appTheme?.replace(/\"/gi, '');
-  return appThemeValue;
+  return appTheme;
 }
 
 export default getServerAppTheme;
